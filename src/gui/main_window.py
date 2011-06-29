57e5bdc6343c534557f415bca9ed04954383ecab
Kjell Winblad <kjellwinblad@gmail.com>
2011-06-29 08:34:02 +0200

    Started creating GUI for example creation.

---
+++ src/gui/main_window.py
@@ -0,0 +1,54 @@
+'''
+Created on Jun 29, 2011
+
+@author: kjellw
+'''
+
+from javax.swing import JFrame
+from javax.swing import BoxLayout
+from javax.swing import JButton
+from javax.swing import JPanel
+from java.awt import BorderLayout
+
+class MainWindow(JFrame):
+    '''
+    The main window of the GUI
+    '''
+    
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        super(MainWindow, self).__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        #Set up window
+        self.setTitle("HandReco")
+        self.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
+        self.setLocationRelativeTo(None)
+        self.setLayout(BoxLayout(self.getContentPane(), BoxLayout.Y_AXIS))
+        self.setSize(300,300)
+        #Add content to window
+        record_examples_panel = JPanel(BorderLayout())
+        record_examples_panel.add(JButton("Record Examples"), BorderLayout.CENTER)
+        self.add(record_examples_panel)
+        #
+        train_model_panel = JPanel(BorderLayout())
+        train_model_panel.add(JButton("Train Model"), BorderLayout.CENTER)
+        self.add(train_model_panel)
+        #
+        classify_examples_panel = JPanel(BorderLayout())
+        classify_examples_panel.add(JButton("Classify Examples"), BorderLayout.CENTER)
+        self.add(classify_examples_panel)
+        #
+        classify_character_panel = JPanel(BorderLayout())
+        classify_character_panel.add(JButton("Classify Character"), BorderLayout.CENTER)
+        self.add(classify_character_panel)
+        #Show
+        self.setVisible(True)
+
+
+if __name__ == '__main__':
+    MainWindow()
